b52207fb0e73e9a2f1f464bfded72fabcd2c40c8
Mateus Gomes de Oliveira <mateusgomesdeoliveira335@gmail.com>
2022-10-10 21:37:42 -0300

    Aprendendo mais sobre metodos

--- tipos_variaveis/src/Metodos.java
+++ tipos_variaveis/src/Metodos.java
@@ -1,5 +1,9 @@
 public class Metodos {
     public static void main(String[] args) {
         // METODOS OU FUNÇOES 
+        // deve ser nomeado como verbo e seguir o padrao camelcase
+        // findById metodo em ingles
+        
+        
     }
 }
